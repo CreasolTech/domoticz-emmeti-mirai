0ca97920d06fb3720d4f4db0c99bb143e778f581
CreasolTech <linux@creasol.it>
2025-02-07 09:58:07 +0100

    Different management of serial port: 2 accesses in exclusive mode, and 1 in non-exclusive mode

--- plugin.py
+++ plugin.py
@@ -111,6 +111,7 @@ class BasePlugin:
         self.rs485.serial.parity = minimalmodbus.serial.PARITY_EVEN
         self.rs485.serial.stopbits = 1
         self.rs485.serial.timeout = 0.1
+        self.rs485.serial.write_timeout = 0 # used in case of problem opening serial device: 0 => return immediately in case of error writing port
         self.rs485.serial.exclusive = True # Fix From Forum Member 'lost'
         self.rs485.debug = True
         self.rs485.mode = minimalmodbus.MODE_RTU
